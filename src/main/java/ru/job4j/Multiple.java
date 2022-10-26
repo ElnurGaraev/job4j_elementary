35fa693b9ac66ee58f2025c55fcbd38557b97b60
User <egaraev88@mail.ru>
2022-10-26 23:13:28 +0400

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -2,6 +2,7 @@ package ru.job4j;
 
 public class Multiple {
     public static void main(String[] args) {
+
         System.out.println("Garaev Elnur");
         System.out.println("26.10.2022");
         System.out.println("1*1=1");
