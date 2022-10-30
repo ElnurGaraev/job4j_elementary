c045864842cd50abfe55590fea49019ce3c8b738
User <egaraev88@mail.ru>
2022-10-30 22:22:05 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,32 +2,37 @@ package ru.job4j.converter;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-        float rsl = value / 70;
-        return rsl;
+        return value / 70;
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value / 60;
-        return rsl;
+        return value / 60;
     }
 
     public static float dollarToRuble(float value) {
-        float rls = value * 60;
-        return rls;
+       return value * 60;
     }
 
     public static float euroToRuble(float value) {
-        float rls = value * 70;
-        return rls;
+        return value * 70;
     }
 
     public static void main(String[] args) {
+        float expectedEuro = 2;
         float euro = Converter.rubleToEuro(140);
+        boolean passedEuro = expectedEuro == euro;
+        float expectedDollar = 3;
         float dollar = Converter.rubleToDollar(140);
+        boolean passedDollar = expectedDollar == dollar;
+        float expectedDolRub = 60;
         float dollarRuble = Converter.dollarToRuble(1);
+        boolean passedDolRub = expectedDolRub == dollarRuble;
+        float expectedEuRub = 70;
         float euroRuble = Converter.euroToRuble(1);
-        System.out.println("140 rubles are " + euro + " euro and " + dollar + " dollar.");
-        System.out.println("1 dollar is " + dollarRuble + " рублей.");
-        System.out.println("1 euro is " + euroRuble + " рублей.");
+        boolean passedEuRub = expectedEuRub == euroRuble;
+        System.out.println("140 rubles are " + euro + ". Test result is " + passedEuro + ".");
+        System.out.println("140 rubles are " + dollar + ". Test result is " + passedDollar + ".");
+        System.out.println("1 dollar is " + dollarRuble + " рублей. Test is " + passedDolRub + ".");
+        System.out.println("1 euro is " + euroRuble + " рублей.Test is " + passedEuRub + ".");
     }
 }
