08ed513fd6b3e4bf033bad5a75836c739dbeae23
User <egaraev88@mail.ru>
2022-11-12 17:36:32 +0300

    6.5.2. Поиск минимума в диапазоне. [#179804]

--- src/test/java/ru/job4j/array/MinDiapasonTest.java
+++ src/test/java/ru/job4j/array/MinDiapasonTest.java
@@ -3,8 +3,6 @@ package ru.job4j.array;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
-
 import static org.junit.Assert.*;
 
 public class MinDiapasonTest {
